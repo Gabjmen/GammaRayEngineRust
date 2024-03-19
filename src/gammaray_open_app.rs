8aedc57a2285bdfa9ba7b082efee2aed04490c9c
Adriandev16 <adrian_amarandei@beenear.com>
2024-03-19 19:17:33 +0200

    open dialog wip

--- src/gammaray_open_app.rs
+++ src/gammaray_open_app.rs
@@ -1,7 +1,11 @@
 #![allow(non_snake_case)]
 
+use std::{fs::File, result, path::Path, path::PathBuf};
+
 use eframe::{egui, NativeOptions, run_native};
 use egui::{Button, CentralPanel, Context, Grid, Label, RichText, SidePanel, TextEdit, TopBottomPanel, Ui};
+
+use egui_file_dialog::FileDialog;
 
 pub fn open_project_window() {
     let native_options = NativeOptions::default();
@@ -10,6 +14,8 @@ pub fn open_project_window() {
 
 #[derive(Default)]
 pub struct MyEguiApp {
+    file_dialog: FileDialog,
+    selected_file: Option<PathBuf>,
     entry_title: String,
 }
 
@@ -17,12 +23,24 @@ impl MyEguiApp {
     pub fn new(cc: &eframe::CreationContext<'_>) -> Self {
         Self {
             entry_title: Default::default(),
+            file_dialog: FileDialog::new(),
+            selected_file: None,
         }
     }
 }
 
-fn my_button(ui: &mut Ui) {
-    if ui.button("Browse..").clicked() {
+// pub fn browse_button(ui: &mut Ui) {
+    
+// }
+
+pub fn open_button(ui: &mut Ui) {
+    if ui.button("Open").clicked() {
+        println!("Button clicked!");
+    }
+}
+
+pub fn cancel_button(ui: &mut Ui) {
+    if ui.button("Cancel").clicked() {
         println!("Button clicked!");
     }
 }
@@ -42,15 +60,35 @@ impl eframe::App for MyEguiApp {
                     ui.label("Project Name");
                     ui.add(TextEdit::singleline(&mut self.entry_title));
                 });
+                ui.end_row();
 
                 ui.horizontal(|ui| {
-                    my_button(ui);
+                    if ui.button("Browse..").clicked() {
+                        self.file_dialog.select_directory();
+                        println!("Button clicked!");
+                    }
+                    open_button(ui);
+                    cancel_button(ui);
                 });
+                ui.end_row();
             });
         });
 
         CentralPanel::default().show(ctx, |ui|{
-            ui.add(TextEdit::singleline(&mut self.entry_title).hint_text("Search"))
+            ui.add(TextEdit::singleline(&mut self.entry_title).hint_text("Search"));
+            CentralPanel::default().show(ctx, |ui| {
+                if ui.button("Select file").clicked() {
+                    // Open the file dialog to select a file.
+                    self.file_dialog.select_file();
+                }
+    
+                ui.label(format!("Selected file: {:?}", self.selected_file));
+    
+                // Update the dialog and check if the user selected a file
+                if let Some(path) = self.file_dialog.update(ctx).selected() {
+                    self.selected_file = Some(path.to_path_buf());
+                }
+            });
         });
     }
 }
